6eda4ce799ae3e6acd8ce57239f6413f414083cf
ninhcongluc <ninhconglucit@gmail.com>
2024-06-09 12:23:45 +0700

    resolve conflict

--- src/configs/send-mail.ts
+++ src/configs/send-mail.ts
@@ -1,19 +1,23 @@
 import nodemailer from 'nodemailer';
+require('dotenv').config();
 
 class EmailService {
-  private transporter: any;
+  transporter: any;
 
   constructor() {
     this.transporter = nodemailer.createTransport({
       service: 'gmail',
+      host: 'smtp.gmail.com',
+      port: 465,
+      secure: true,
       auth: {
-        user: 'your-email@gmail.com',
-        pass: 'your-password'
+        user: process.env.EMAIL_USER,
+        pass: process.env.EMAIL_PASS
       }
     });
   }
 
-  sendEmail(mailOptions: any): any {
+  sendEmail(mailOptions: any) {
     return this.transporter.sendMail(mailOptions);
   }
 }
--- src/modules/user/user.controller.ts
+++ src/modules/user/user.controller.ts
@@ -1,10 +1,21 @@
+import { Request, Response } from 'express';
 import { StatusCodes } from 'http-status-codes';
 import { UserService } from './user.service';
 import { AppObject } from '../../commons/consts/app.objects';
 
 export class UserController {
   constructor(private readonly userService: UserService) {}
-  async listManagerAccount(req, res) {
+
+  async createManagerAccount(req: Request, res: Response) {
+    try {
+      const newUser = await this.userService.createManagerAccount(req.body);
+      return res.status(201).send({ data: newUser, status: StatusCodes.CREATED });
+    } catch (error) {
+      return res.status(400).send({ error: error.message, status: StatusCodes.BAD_REQUEST });
+    }
+  }
+
+  async listManagerAccount(req: Request, res: Response) {
     try {
       const users = await this.userService.listAccountByRole(AppObject.ROLE_CODE.MANAGER);
       return res.status(200).send({ data: users, status: StatusCodes.OK });
@@ -13,7 +24,37 @@ export class UserController {
     }
   }
 
-  async listTeacherAccount(req, res) {
+  async getDetailManager(req: Request, res: Response) {
+    try {
+      const userId = req.params.id;
+      const user = await this.userService.getDetailUser(userId);
+      return res.status(200).send({ data: user, status: StatusCodes.OK });
+    } catch (error) {
+      return res.status(400).send({ error: error.message, status: StatusCodes.BAD_REQUEST });
+    }
+  }
+
+  async updateManagerAccount(req: Request, res: Response) {
+    try {
+      const id = req.params.id;
+      const updatedUser = await this.userService.updateManagerAccount(id, req.body);
+      return res.status(201).send({ data: updatedUser, status: StatusCodes.CREATED });
+    } catch (error) {
+      return res.status(400).send({ error: error.message, status: StatusCodes.BAD_REQUEST });
+    }
+  }
+
+  async deleteManagerAccount(req: Request, res: Response) {
+    try {
+      const id = req.params.id;
+      const newUser = await this.userService.deleteManagerAccount(id);
+      return res.status(201).send({ data: newUser, status: StatusCodes.CREATED });
+    } catch (error) {
+      return res.status(400).send({ error: error.message, status: StatusCodes.BAD_REQUEST });
+    }
+  }
+
+  async listTeacherAccount(req: Request, res: Response) {
     try {
       const users = await this.userService.listAccountByRole(AppObject.ROLE_CODE.TEACHER);
       return res.status(200).send({ data: users, status: StatusCodes.OK });
@@ -22,7 +63,7 @@ export class UserController {
     }
   }
 
-  async listStudentAccount(req, res) {
+  async listStudentAccount(req: Request, res: Response) {
     try {
       const users = await this.userService.listAccountByRole(AppObject.ROLE_CODE.STUDENT);
       return res.status(200).send({ data: users, status: StatusCodes.OK });
