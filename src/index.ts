a51cb92096c661985a09739f74b1bdf3db3fda94
ninhcongluc <ninhconglucit@gmail.com>
2024-06-14 15:47:15 +0700

    Fix conflict

--- src/index.ts
+++ src/index.ts
@@ -2,6 +2,8 @@ import { Router } from 'express';
 import userRouter from './modules/user/user.route';
 import authRouter from './modules/auth/auth.route';
 import semesterRouter from './modules/semester/semester.route';
+import quizRouter from './modules/quiz/quiz.route';
+import classRouter from './modules/class/class.route';
 
 // Create a new Router instance
 const router = Router();
@@ -12,5 +14,8 @@ router.use('/auth', authRouter);
 router.use('/', userRouter);
 
 router.use('/', semesterRouter);
+router.use('/', quizRouter);
+
+router.use('/', classRouter);
 
 export default router;
