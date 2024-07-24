18be01307201ce38af048f72cabd365ae2a24212
ninhcongluc <ninhconglucit@gmail.com>
2024-07-24 18:37:52 +0700

    Validate quiz

--- src/modules/quiz/quiz.service.ts
+++ src/modules/quiz/quiz.service.ts
@@ -212,7 +212,7 @@ export class QuizService {
       if (!quiz) {
         throw new Error('Quiz not found');
       }
-      if (incomingQuestions.length > 0 && quiz.status === QuizStatus.SUBMITTED) {
+      if (incomingQuestions.length > 0 && isSubmit) {
         await this.validateQAScore(quiz, incomingQuestions);
       }
       const existingQuestions = (await this.listQuestionAnswers(quizId)).questions;
